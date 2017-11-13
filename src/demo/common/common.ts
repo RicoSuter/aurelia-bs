514428b90feaac7b6e8f6ad0db5ea8473b4d8b6c
Rico Suter <mail@rsuter.com>
2017-11-13 10:38:38 +0100

    Improved demo

--- src/demo/common/common.ts
+++ src/demo/common/common.ts
@@ -1,3 +1,20 @@
-export class Common {
+import { BsTextbox } from '../../textbox';
+import { BsSearchbox } from '../../searchbox';
 
+export class Common {
+  textbox: BsTextbox;
+  textarea: BsTextbox;
+  searchbox: BsSearchbox;
+
+  focusTextbox() {
+    this.textbox.focus();
+  }
+
+  focusTextarea() {
+    this.textarea.focus();
+  }
+
+  focusSearchbox() {
+    this.searchbox.focus();
+  }
 }
